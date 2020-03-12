e80bb924bf4b0335862bb23ab189c1a7393b093c
Aurelio <aurelio.hdz.aguilar@gmail.com>
2020-03-12 17:27:31 -0600

    Some tweaks for read more columns and filter phone numbers from excel

--- read_excel.py
+++ read_excel.py
@@ -1,19 +1,36 @@
 import pandas
-
+import re
 
 def read_exce():
     # df = pandas.read_excel('CASETA.xlsx', index_col=[0, 1, 2], skiprows=1, sheet_name='DANUBIO', )
-    df = pandas.read_excel('CASETA.xlsx', usecols="B", skiprows=1, sheet_name='TINTO', )
+    df = pandas.read_excel('CASETA.xlsx', usecols="B:C", skiprows=1, sheet_name='LENA', )
     values = df.values
 
     # print(values)
     for row in values:
-        for element in row:
-            element = element.split("#")
-            print(element)
-            if len(element) != 2:
-                print("ERROR ")
-                return
+        address = row[0]
+        address = address.split("#")
+        print(address)
+        if len(address) != 2:
+            print("ERROR ")
+            return
+        phone = row[1]
+        if phone:
+            phone = str(phone)
+            # print(f"{phone} tipo {type(phone)}")
+            phone = re.sub("\D", "", phone)
+            phone = phone[:10]
+            print(phone)
+        # print(row)
+
+
+        # for element in row:
+            # element[0]
+            # element = element.split("#")
+            # print(element)
+            # if len(element) != 2:
+            #     print("ERROR ")
+            #     return
         print()
     # print(df)
 
